e99c04d322a153b0f710e0b2f84c60daf3da4663
Abhishek Adagale <126813011+abhishekadagale2509@users.noreply.github.com>
2025-07-26 01:26:34 +0530

    Add files via upload

---
+++ if_else.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void main(){
+    int a;
+    cout<<"enter no";
+    cin>>a;
+    if(a>0){
+        cout<<"positive no";
+
+    }
+    else{
+        cout<<"negative no";
+    }
+}
+/*
+int a,b;
+cin>>a>>b;
+if(a>b){
+cout<<"a is greater";
+}
+else{
+cout<<"b is greater";
+}*/

ba728ca51ec322ec31410fbafe20cfbb4d2c9649
Abhishek Adagale <126813011+abhishekadagale2509@users.noreply.github.com>
2025-08-06 22:36:48 +0530

    Add files via upload

---
+++ functionHomework.cpp
@@ -0,0 +1,12 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+void update(int a)
+{
+    a=a/2;
+}
+int main() {
+    int a=10;
+    update(a);
+    cout<<a;
+}

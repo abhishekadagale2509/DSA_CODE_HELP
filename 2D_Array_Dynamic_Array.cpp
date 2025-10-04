c3f7d1c5de5c7340fef58fb2a7261ef1beca0cdb
Abhishek Adagale <126813011+abhishekadagale2509@users.noreply.github.com>
2025-10-04 23:04:09 +0530

    Add files via upload

---
+++ 2D_Array_Dynamic_Array.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    // allocate 2D array
+    int **arr = new int*[n];
+    for (int i = 0; i < n; i++) {
+        arr[i] = new int[n];
+    }
+
+    // input elements
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    // output elements
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // free memory
+    for (int i = 0; i < n; i++) {
+        delete[] arr[i];
+    }
+    delete[] arr;
+
+    return 0;
+}
